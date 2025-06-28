94f18a07432c01f9fabbf5ec18a1d589986a8af1
sasha-nedz <sashanedzel@gmail.com>
2025-06-28 13:52:38 +0100

    Update thermostat.js

--- vercel-thermostat-api/api/thermostat.js
+++ vercel-thermostat-api/api/thermostat.js
@@ -1,17 +1,11 @@
-// example: serverless endpoint
-export async function handler(event, context) {
-  const res = await fetch("https://api.thingspeak.com/channels/2992938/fields/2/last.json?api_key=T4IH7M7HYC5JEC77");
-  const data = await res.json();
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
 
-  const json = {
-    thermostat001: {
-      Temperature: parseFloat(data.field2)
-    }
-  };
-
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(json)
-  };
+  res.status(200).json({
+    thermostat001: { Temperature: 74 },
+    light001: { "Is On": true },
+    lock001: { "Is Locked": false }
+    // Add more devices here
+  });
 }
