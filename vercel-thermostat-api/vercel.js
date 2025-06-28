2ea8172ce54ddf10037b190b2c9df49d505d9b24
sasha-nedz <sashanedzel@gmail.com>
2025-06-28 13:56:05 +0100

    Delete vercel-thermostat-api/vercel.js

--- vercel-thermostat-api/vercel.js
+++
@@ -1,9 +0,0 @@
-{
-  "version": 2,
-  "builds": [
-    { "src": "api/thermostat.js", "use": "@vercel/node" }
-  ],
-  "routes": [
-    { "src": "/api/thermostat", "dest": "api/thermostat.js" }
-  ]
-}
